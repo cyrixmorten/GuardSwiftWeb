effa15c798b2b9711ec2d38c066ad03c1059f817
cyrixmorten <cyrixmorten@gmail.com>
2016-01-24 23:23:53 +0100

    3.0.7 add logo to static reports

--- app/js/controllers/report.js
+++ app/js/controllers/report.js
@@ -3,24 +3,32 @@
 var controllerModule = angular.module('GuardSwiftApp.controllers');
 
 
-controllerModule.controller('ReportCtrl', ['$scope', 'ParseEventLog', 'reportObject',
-		function($scope, ParseEventLog, reportObject) {
+controllerModule.controller('ReportCtrl', ['$scope', 'ParseEventLog', 'ParseReport', 'reportObject',
+		function($scope, ParseEventLog, ParseReport, reportObject) {
 
 			$scope.report = reportObject;
 
-			if (reportObject.eventLogs) { 
+
+			var parseReport = ParseReport.getParseObject(reportObject);
+
+			var owner = parseReport.get('owner');
+			var staticReportSettings = owner.get('staticReportSettings');
+			var headerLogo1 = staticReportSettings.get('headerLogo1');
+			$scope.headerLogo1Url = headerLogo1._url;
+
+			if (reportObject.eventLogs) {
 				$scope.entries = reportObject.eventLogs;
 			}
 
-			// backwards compatibility < 3.0.0	
+			// backwards compatibility < 3.0.0
 			if (reportObject.reportEntries) {
 				$scope.entries = [];
 				angular.forEach(reportObject.reportEntries, function(reportEntry) {
 					reportEntry.taskEvent = reportEntry.task_event;
 					$scope.entries.push(reportEntry);
 				});
-				$scope.entries = reportObject.reportEntries; 
-			}			
+				$scope.entries = reportObject.reportEntries;
+			}
 
 			console.log(reportObject);
 
@@ -31,18 +39,18 @@ controllerModule.controller('AlarmReportCtrl', ['$scope', 'scopedAlarm',
 		function($scope, scopedAlarm) {
 
 			var report = scopedAlarm.report;
-			
+
 			$scope.securityLevel = scopedAlarm.securityLevel;
-			
+
 			$scope.timeReactionMinutes = moment(scopedAlarm.timeStarted).diff(moment(scopedAlarm.timeStartedDriving), 'minutes');
 			$scope.timeTotalMinutes = moment(scopedAlarm.timeEnded).diff(moment(scopedAlarm.timeStartedDriving), 'minutes');;
-			
+
 			console.log(scopedAlarm.timeStartedDriving);
 			console.log(scopedAlarm.timeEnded);
 			console.log($scope.timeReactionMinutes);
 			console.log($scope.timeTotalMinutes);
 
-			
+
 			$scope.report = report.report;
 			$scope.internal = report.internal;
 
