167a1bc49dfb241c2985d19e4e7b538102d948bd
Morten Rasmussen <cyrixmorten@MacBook-Pro-4.local>
2016-10-25 20:08:21 +0200

    Added parse-server

--- server/app.js
+++ server/app.js
@@ -16,10 +16,58 @@ var express = require('express'),
 
 var app = module.exports = express();
 
+/**
+ * SETUP PARSE SERVER
+ */
+
+var S3Adapter = require('parse-server').S3Adapter;
+var ParseServer = require('parse-server').ParseServer;
+
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+
+var parseApi = new ParseServer({
+  databaseURI: databaseUri, //'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN, // || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID,
+  fileKey: process.env.FILE_KEY,
+  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  filesAdapter: new S3Adapter(
+      process.env.S3_KEY,
+      process.env.S3_SECRET,
+      process.env.S3_BUCKET,
+      { directAccess: true }
+  )
+});
+
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Serve the Parse API on the /parse URL prefix
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, parseApi);
+
+
+var port = process.env.PORT || 1337;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+  console.log('parse-server-example running on port ' + port + '.');
+});
+
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
 
 /**
- * Configuration
+ * CONFIGURE SERVER
  */
+
 
 var clientPath = path.join(__dirname, '../app');
 // all environments
