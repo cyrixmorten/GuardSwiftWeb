37e7ad2688aad75af792644076bc1e3ff548118a
Morten Rasmussen <cyrixmorten@MacBook-Pro-4.local>
2016-10-27 14:58:04 +0200

    Temporary disable all ported cloud code

--- server/cloud/main.js
+++ server/cloud/main.js
@@ -1,36 +1,36 @@
-require("./global.js");
-
-require("./file/fileDelete.js");
-require("./file/fileToDatauri.js");
-
-require("./pdf/definitions/docDefaults.js");
-require("./pdf/definitions/regularReport.js");
-require("./pdf/definitions/staticReport.js");
-require("./pdf/definitions/taskReport.js");
-require("./pdf/reportToMail.js");
-require("./pdf/reportToPDF.js");
-require("./pdf/reportUtils.js");
-
-require("./scheduled/dailyMailReports.js");
-require("./scheduled/dailyTasksReset.js");
-
-require("./triggers/client.js");
-require("./triggers/circuit.js");
-require("./triggers/circuitunit.js");
-require("./triggers/districtwatch.js");
-require("./triggers/districtwatchunit.js");
-require("./triggers/districtwatchclient.js");
-require("./triggers/eventlog.js");
-require("./triggers/eventtype.js");
-require("./triggers/user.js");
-require("./triggers/task.js");
-
-require("./utils/geocode.js");
-require("./utils/mailing.js");
-require("./utils/pdf.js");
-
-require("./alarm/parse.js");
-
-
-
-
+// require("./global.js");
+//
+// require("./file/fileDelete.js");
+// require("./file/fileToDatauri.js");
+//
+// require("./pdf/definitions/docDefaults.js");
+// require("./pdf/definitions/regularReport.js");
+// require("./pdf/definitions/staticReport.js");
+// require("./pdf/definitions/taskReport.js");
+// require("./pdf/reportToMail.js");
+// require("./pdf/reportToPDF.js");
+// require("./pdf/reportUtils.js");
+//
+// require("./scheduled/dailyMailReports.js");
+// require("./scheduled/dailyTasksReset.js");
+//
+// require("./triggers/client.js");
+// require("./triggers/circuit.js");
+// require("./triggers/circuitunit.js");
+// require("./triggers/districtwatch.js");
+// require("./triggers/districtwatchunit.js");
+// require("./triggers/districtwatchclient.js");
+// require("./triggers/eventlog.js");
+// require("./triggers/eventtype.js");
+// require("./triggers/user.js");
+// require("./triggers/task.js");
+//
+// require("./utils/geocode.js");
+// require("./utils/mailing.js");
+// require("./utils/pdf.js");
+//
+// require("./alarm/parse.js");
+//
+//
+//
+//
