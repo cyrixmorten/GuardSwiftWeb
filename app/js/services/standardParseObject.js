b6a1dec11394520aebe62a51404c7777d13e4217
Morten Rasmussen <cyrixmorten@gmail.com>
2019-03-04 16:30:16 +0100

    Update how archived objects are excluded

--- app/js/services/standardParseObject.js
+++ app/js/services/standardParseObject.js
@@ -572,7 +572,7 @@ app
                                                 .current());
 
 
-                                        query.notEqual('archive', true);
+                                        query.notEqualTo('archive', true);
 
                                     }
 
